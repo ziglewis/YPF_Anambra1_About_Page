70612f414d9aabe892e6d2be574597e724ebdab0
ziglewis <ziglewis48@gmail.com>
2023-07-21 14:59:01 +0100

    modified css

--- index.js
+++ index.js
@@ -115,6 +115,22 @@ let poUpOnAndOff = () => {
          newsLetterForm.reset()
          
        });
+
+
+
+   // HANDLING THE SORTING OF THE BLOG WHEN IT IS LINKED TO FROM THE FOOTER BLOG SECTION
+  
+   let myAllFooterBlogLinks = document.querySelectorAll (".blog a")
+  
+   myAllFooterBlogLinks.forEach(function(blogCategory) {
+       
+       let categoryName = blogCategory.textContent.split(" ")[1].toLowerCase()+"Blog"
+       blogCategory.addEventListener("click", function(){
+           window.location.href =  `
+           https://ziglewis.github.io/YPF_Anambra_Blog_Page/?${categoryName}=true 
+           `     
+           })  }) 
+
       
 
       
